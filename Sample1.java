c35fb297867c03049c89cbf7fb3d990e8e8d04df
Yukie Koga <inyoyogi@gmail.com>
2018-09-19 22:43:02 +0900

    ボーダーペインについて学習

--- Sample1.java
+++ Sample1.java
@@ -4,10 +4,11 @@ import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.geometry.*;
 
 public class Sample1 extends Application {
     
-    private Label lb;
+    private Button bt[] = new Button[5];
     
     public static void main(String args[]) {
         launch(args);
@@ -16,16 +17,25 @@ public class Sample1 extends Application {
     public void start(Stage stage)throws Exception {
         
         //コントロールの作成
-        lb = new Label();
-        
-        //コントロールの設定
-        lb.setText("いらっしゃいませ");
+        bt[0] = new Button("Top");
+        bt[1] = new Button("Bottom");
+        bt[2] = new Button("Center");
+        bt[3] = new Button("Left");
+        bt[4] = new Button("Right");
         
         //ペインの作成
         BorderPane bp = new BorderPane();
         
         //ペインへの追加
-        bp.setCenter(lb);
+        bp.setTop(bt[0]);
+        bp.setBottom(bt[1]);
+        bp.setCenter(bt[2]);
+        bp.setLeft(bt[3]);
+        bp.setRight(bt[4]);
+        
+        for(int i = 0; i < bt.length; i ++) {
+            bp.setAlignment(bt[i], Pos.CENTER);
+        }
         
         //シーンの作成
         Scene sc = new Scene(bp, 300, 200);
